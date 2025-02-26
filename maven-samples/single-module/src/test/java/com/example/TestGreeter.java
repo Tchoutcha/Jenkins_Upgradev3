7be5237c4f864b15ab992b322961226dbb78b6c2
Stella <83948740+Tchoutcha@users.noreply.github.com>
2025-02-26 10:01:06 +0100

    Update TestGreeter.java
    edit word in second test

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -30,7 +30,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
+    String someone = "HELLO World";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
